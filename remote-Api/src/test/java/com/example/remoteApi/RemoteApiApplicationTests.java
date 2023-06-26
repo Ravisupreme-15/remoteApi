a7f25904eb12cc79547101e57694afa78fa3c3c2
RaviSupreme-15 <supremeravi1819@gmail.com>
2023-06-26 16:40:12 +0530

    call remote api

---
+++ remote-Api/src/test/java/com/example/remoteApi/RemoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remoteApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
